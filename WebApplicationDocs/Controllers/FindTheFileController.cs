cdf8558998bea645c1e5818ede6c42dc3d50aca7
Pranav Kadari <pranav.kadari@zelis.com>
2025-05-11 13:31:52 +0530

    Added "Find The File" function

---
+++ WebApplicationDocs/Controllers/FindTheFileController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System.IO;
+using System.IO.Compression;
+using WebApplicationDocs.Models;
+
+namespace WebApplicationDocs.Controllers
+{
+    public class FindTheFileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessFile(FindTheFileModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Message = "Provided input is invalid.";
+                return View("Index", model);
+            }
+
+            try
+            {
+                
+                Directory.CreateDirectory(model.DestPath);
+
+                string[] zipFiles = Directory.GetFiles(model.SourcePath, $"{model.ClientId}*.zip");//client... .zip//change
+                if (zipFiles.Length == 0)
+                {
+                    ViewBag.Message = $"Files for {model.ClientId} not found.";
+                    return View("Index", model);
+                }
+
+                string validFile = null;
+                foreach (var file in zipFiles.OrderByDescending(f => System.IO.File.GetCreationTime(f)))
+                {
+                    string copiedFile = Path.Combine(Path.GetTempPath(), Path.GetFileName(file));
+                    System.IO.File.Copy(file, copiedFile, true);
+
+                    string unzipDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                    Directory.CreateDirectory(unzipDir);
+                    ZipFile.ExtractToDirectory(copiedFile, unzipDir);
+
+                    string[] unZippedFiles = Directory.GetFiles(unzipDir, "*.*");
+                    string[] lines = System.IO.File.ReadAllLines(unZippedFiles[0]);
+                    string[] firstLineContent = lines[0].Split("\t");
+                    string filesDocumentType = firstLineContent[22];
+
+                    int FilePaymentCount = int.Parse(lines[lines.Length - 1].Split("\t")[2].Substring(19));
+
+                    if (filesDocumentType == model.DocumentType && FilePaymentCount + 1 >= model.PaymentFileNum)
+                    {
+                        validFile = unZippedFiles[0];
+                        
+                        string tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                        Directory.CreateDirectory(tempDir);
+
+                        // Copy the valid file to the temporary directory
+                        string tempFilePath = Path.Combine(tempDir, Path.GetFileName(validFile));
+                        System.IO.File.Copy(validFile, tempFilePath, true);
+
+                        // Create the zip file from the temporary directory
+                        string destinationFilePath = Path.Combine(model.DestPath, Path.GetFileName(validFile) + ".zip");
+                        destinationFilePath = destinationFilePath.Replace(".Docs", "");
+                        ZipFile.CreateFromDirectory(tempDir, destinationFilePath);
+
+                        // Clean up the temporary directory
+                        Directory.Delete(tempDir, true);
+                        ViewBag.Message = $"{model.ClientId} Files with document type {model.DocumentType} and number of payment files more than {model.PaymentFileNum} is pasted at {model.DestPath}.";
+                        break;
+                        //1.Path.GetFileName(validFile): Extracts the file name(e.g., example.txt) from the validFile path.
+                        //2.Path.Combine(model.DestPath, ...): Combines the destination directory(model.DestPath) with the file name to create the full destination file path.
+                        //3.System.IO.File.Copy: Copies the file from validFile(source) to the constructed destinationFilePath.
+                        //ProcessFile(validFile, tinNumbers, unzipDir, model.DestPath, model.PaymentFileNum, model.ReplacementSuffix);
+
+                    }
+                    Directory.Delete(unzipDir, true);
+                }
+
+                if (validFile == null)
+                {
+                    ViewBag.Message = $"{model.ClientId} Files with document type {model.DocumentType} not found.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Error: " + ex.Message;
+            }
+
+            return View("Index", model);
+        }
+
+    }
+}
--- WebApplicationDocs/Models/FindTheFileModel.cs
+++ WebApplicationDocs/Models/FindTheFileModel.cs
@@ -2,8 +2,9 @@
 
 namespace WebApplicationDocs.Models
 {
-    public class FileProcessingModel
+    public class FindTheFileModel
     {
+
         [Required]
         public string SourcePath { get; set; }
 
@@ -14,9 +15,6 @@ namespace WebApplicationDocs.Models
         [StringLength(3, MinimumLength = 3, ErrorMessage = "Document type must be exactly 3 characters.")]
         public string DocumentType { get; set; }
 
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "Replacement suffix must be exactly 2 characters.")]
-        public string ReplacementSuffix { get; set; }
-
         [Required]
         public string DestPath { get; set; }
 
@@ -24,15 +22,7 @@ namespace WebApplicationDocs.Models
         [Range(1, int.MaxValue, ErrorMessage = "Enter a valid number of Payment Files.")]
         public int PaymentFileNum { get; set; }
 
-        public IFormFile ExcelFile { get; set; }
-    }
 
-    public class FileProcessingModelWithoutExcelAndSuffix : FileProcessingModel
-    {
-        [Required(AllowEmptyStrings = true)]
-        public new string ReplacementSuffix { get; set; } = null;
-
-        [Required(AllowEmptyStrings = true)]
-        public new IFormFile ExcelFile { get; set; } = null;
     }
 }
+
