275c3aa127cd71bcb82c3837bc8141dea8ae06f8
Pranav Kadari <pranav.kadari@zelis.com>
2025-07-11 15:49:23 +0530

    Making changes to open edge directly when clicking on .exe and s-final >Dynamic

--- WebApplicationDocs/Program.cs
+++ WebApplicationDocs/Program.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +14,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,5 +27,13 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Automatically open the browser to the app's URL
+var url = "http://localhost:5000";
+Task.Run(() => Process.Start(new ProcessStartInfo
+{
+    FileName = url,
+    UseShellExecute = true
+}));
 
 app.Run();
